93308eac24b825a401e0ed1d0645352b8dac4deb
criptometre <130353145+criptometre@users.noreply.github.com>
2026-03-09 14:40:58 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:23:49.000Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-09T11:40:59.365Z
+Commit: Patch API endpoint error handling
