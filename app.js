78a4877f27bb2ad10664306c6afe39723a81838c
criptometre <130353145+criptometre@users.noreply.github.com>
2026-03-09 14:14:55 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:14:52.729Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-09T11:14:56.260Z
+Commit: Fix CSS styling bug on mobile devices
