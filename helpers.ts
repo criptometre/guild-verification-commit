7ad9f8e4c8fbb71d6084100c304d1773fdc5c5ec
criptometre <130353145+criptometre@users.noreply.github.com>
2026-03-09 14:48:16 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:45:18.519Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-09T11:48:17.129Z
+Commit: Fix memory leak in event handlers
