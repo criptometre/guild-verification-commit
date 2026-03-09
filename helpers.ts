42374170c3c6420036e1dfd492b3449656ffb88d
criptometre <130353145+criptometre@users.noreply.github.com>
2026-03-09 14:45:17 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:45:08.052Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-09T11:45:18.519Z
+Commit: Introduce real-time notifications
