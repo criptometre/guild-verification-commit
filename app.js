a5248bcf327f52d40d898cadabe9badb964c046b
criptometre <130353145+criptometre@users.noreply.github.com>
2026-03-09 14:48:11 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:45:09.170Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-09T11:48:11.794Z
+Commit: Fix broken links in documentation
